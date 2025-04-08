8ec93154cf1b8cd0a0e05cea3af52a1e3c7f0594
alla0810 <kyosook.shin@gmail.com>
2025-04-08 12:14:56 -0500

    reorganize

--- 1_build_cloud/2_createThings/createThing-Cert.py
+++ 1_build_cloud/2_createThings/createThing-Cert.py
@@ -9,8 +9,12 @@ import string
 ################################################### Parameters for Thing
 thingArn = ''
 thingId = ''
-defaultPolicyName = 'policy1'
+defaultPolicyName = 'GreengrassV2IoTThingPolicy'
+defaultThingGroupName = 'MyGreengrassCoreGroup'
+defaultThingGroupArn = 'arn:aws:iot:us-east-1:058264177579:thinggroup/MyGreengrassCoreGroup'
 ###################################################
+
+thingClient = boto3.client('iot')
 
 def createThing(thingName):
   global thingClient
@@ -18,35 +22,40 @@ def createThing(thingName):
       thingName = thingName
   )
   data = json.loads(json.dumps(thingResponse, sort_keys=False, indent=4))
-  for element in data: 
-    if element == 'thingArn':
-        thingArn = data['thingArn']
-    elif element == 'thingId':
-        thingId = data['thingId']
-    createCertificate(thingName)
+  thingArn = data.get('thingArn','')
+  thingId = data.get('thingId', '')
+
+  createCertificate(thingName)
 
 def createCertificate(thingName):
 	global thingClient
+
+	certDir = './certificates'
+	certFile = os.path.join(certDir, f'{thingName}_cert.pem')
+
+	if os.path.exists(certFile):
+		print(f"Certificate for {thingName} already exists.  Skipping certificate creation")
+		return
+
 	certResponse = thingClient.create_keys_and_certificate(
 			setAsActive = True
 	)
+	
 	data = json.loads(json.dumps(certResponse, sort_keys=False, indent=4))
-	for element in data: 
-			if element == 'certificateArn':
-					certificateArn = data['certificateArn']
-			elif element == 'keyPair':
-					PublicKey = data['keyPair']['PublicKey']
-					PrivateKey = data['keyPair']['PrivateKey']
-			elif element == 'certificatePem':
-					certificatePem = data['certificatePem']
-			elif element == 'certificateId':
-					certificateId = data['certificateId']
-							
-	with open(os.path.join('certificates', '') + thingName + '_public.key', 'w') as outfile:
+	certificateArn = data.get('certificateArn', '')
+	PublicKey = data.get('keyPair', {}).get('PublicKey', '')
+	PrivateKey = data.get('keyPair', {}).get('PrivateKey', '')
+	certificatePem = data.get('certificatePem', '')
+	certificateId = data.get('certificateId', '')
+
+	if not os.path.exists(certDir):
+		os.mkdir(certDir)
+						
+	with open(os.path.join(certDir, f'{thingName}_public.key'), 'w') as outfile:
 			outfile.write(PublicKey)
-	with open(os.path.join('certificates', '') + thingName + '_private.key', 'w') as outfile:
+	with open(os.path.join(certDir, f'{thingName}_private.key'), 'w') as outfile:
 			outfile.write(PrivateKey)
-	with open(os.path.join('certificates', '') + thingName + '_cert.pem', 'w') as outfile:
+	with open(os.path.join(certDir, f'{thingName}_cert.pem'), 'w') as outfile:
 			outfile.write(certificatePem)
 
 	response = thingClient.attach_policy(
@@ -58,21 +67,20 @@ def createCertificate(thingName):
 			principal = certificateArn
 	)
 	response = thingClient.add_thing_to_thing_group(
-		    thingGroupName='ThingGroup1',
-			thingGroupArn='arn:aws:iot:us-east-1:058264177579:thinggroup/ThingGroup1',
+		    thingGroupName=defaultThingGroupName,
+			thingGroupArn=defaultThingGroupArn,
 			thingName=thingName,
 			thingArn=thingArn
 	)
 
 
-certDir = "./certificates"
-if os.path.exists(certDir):
-	shutil.rmtree(certDir)
-os.mkdir(certDir)
-
-thingClient = boto3.client('iot')
-
 def main(): 
+	certDir = './certificates'
+	if os.path.exists(certDir):
+		shutil.rmtree(certDir)
+	os.mkdir(certDir)	
+
+
 	for i in range(5):
 		thingName = 'vehicle_' + str(i)
 
